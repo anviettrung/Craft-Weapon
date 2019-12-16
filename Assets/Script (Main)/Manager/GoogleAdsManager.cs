833ede0787db9f45070b5b6460603b6cbb61fe98
anviettrung <an.viettrung.m@gmail.com>
2019-12-16 15:52:41 +0700

    Update reponsive UI

---
+++ Assets/Script (Main)/Manager/GoogleAdsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleAdsManager : MonoBehaviour
+{
+	private RewardedAd rewardedAd;
+
+	public void Start()
+	{
+		MobileAds.Initialize(initStatus => { });
+
+		rewardedAd = new RewardedAd("ca-app-pub-3940256099942544/1712485313");
+
+		AdRequest request = new AdRequest.Builder().Build();
+		rewardedAd.LoadAd(request);
+	}
+
+	public void UserChoseToWatchAd()
+	{
+		Debug.Log("Watch Ads");
+
+		if (rewardedAd.IsLoaded()) {
+			rewardedAd.Show();
+		}
+	}
+}
